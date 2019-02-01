3db06cce1d3d76c143f8412259889d45248ddfe9
Morsal Niyaz <morsalniyaz@gmail.com>
2019-01-31 19:22:22 -0800

    anlysis

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+from templates.TAnalyzer import TwitterNews
+import requests
 
 app = Flask(__name__)
 
@@ -8,14 +10,41 @@ def my_form():
 
 @app.route('/twitter', methods=['GET'])
 def my_form_post():
-	text = request.args.get('text')
-	return render_template('index.html', text=text)
+    text = request.args.get('text')
+    currenttweets = get_tweets(text)
+    print(currenttweets)
+    return render_template('index.html', text=text, currenttweets=currenttweets)
+
 
 #HELPER FUNCTION
 def get_tweets(searchitem):
     url = 'https://api.twitter.com/1.1/search/tweets.json?q=%23{}&result_type=recent'.format(searchitem)
-    headers = {'authorization': 'Bearer <BEARERTOKEN>'}
+    headers = {'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAKKX9QAAAAAAqPRkW0wMLgOF29YuPvrLJLMETI0%3D3x0GtJuEXLEIzYvRKG8fjPB02vwQJp7xd5Z2rOBWY4jGmnD7Ma'}
     res = requests.get(url, headers=headers)
+
+    status = res.json()['statuses'] #inside the res(responses there is a bunch of information about the tweet and we want to get the text so inside the statues there is text so we first get the statuses try print out the repsonses to see what's inside then print out the statues to see if the text is actually in there)
+    
+    tweetlist = [] #a list that will have the sentiment and the text for each tweet 
+
+    twitter_analyzer = TwitterNews() #you are calling the class TwitterNew from TAnalyzer.py
+    
+    for item in status: #now we are trying to get the text that is inside the status 
+        #print(item['text'])
+        tweet = item['text']
+        sentimenttweet = twitter_analyzer.analyze_news(tweet) #you are calling the sentiment part from the TwitterNews class called analyze_news
+        #print(sentimenttweet)
+        # {'sentiment': 'positive', 'tweet': 'i love twitter'} -structure of a dictionary
+        actualtweets = {}  #a dictionary
+        actualtweets["sentiment"] = sentimenttweet #the key - the right hand side is calling the sentiment tweets made from above 
+        actualtweets["tweet"] = tweet #the value
+        #print(actualtweets) 
+
+        tweetlist.append(actualtweets) #adding the acutaltweets dictionary into the tweetlist 
+
+    
+    return tweetlist
+
 
 if __name__ == '__main__':
   app.run() 
+
--- templates/TAnalyzer.py
+++ templates/TAnalyzer.py
@@ -1,71 +1,47 @@
 #Importing the top 5 stories and top 5 comments
+with open('/Users/MorsalNiyaz/Development/polyglot/cohort-1/data/positive_words.txt', 'r') as open_pos:
+    positive_vocab=open_pos.readlines()
+    positive_vocab = list(map(lambda word: word.replace("\n", ""), positive_vocab)) #this take away the "/n" at the end of every tweet or comment
 
-import requests
+with open('/Users/MorsalNiyaz/Development/polyglot/cohort-1/data/negative_words.txt', 'r') as open_neg:
+    negative_vocab=open_neg.readlines()
+    negative_vocab = list(map(lambda word: word[:-1], negative_vocab)) #another way of removing the "/n" at the end of a tweet
+
 
 class TwitterNews:
-    def __init__(self, positive_vocab, negative_vocab):
-        self.positive_vocab = positive_vocab
+    def __init__(self):
+        self.positive_vocab = positive_vocab #you are calling the vocab from above
         self.negative_vocab = negative_vocab
 
     
-    def analyze_news(self, tweets):
-        words = tweets.split()
+    def analyze_news(self, tweet):
+        
+        tweet = tweet.lower() #these are making the tweets on twitter lowercase, taking away #,@ etc. and replacing it with ""(which is blank)
+        tweet = tweet.replace("#", "")  #so that our analyzer can give us an analysis without being confused
+        tweet = tweet.replace("@","")
+        tweet = tweet.replace("$", "")
+        words = tweet.split()
+        print(self.positive_vocab) #need to put the self.
+        print(self.negative_vocab)
 
         num_positive_vocab = 0 
         num_negative_vocab = 0 
         for word in words:
-         
-            if word in positive_vocab:
-                num_positive_vocab += 1
-            elif word in negative_vocab:
+            if word in self.positive_vocab:
+                num_positive_vocab += 1 
+            elif word in self.negative_vocab:
                 num_negative_vocab += 1 
+                print(num_negative_vocab)
+               
 
         if num_positive_vocab > num_negative_vocab:
+            
             return "positive"
 
         elif num_positive_vocab < num_negative_vocab:
             return "negative"
         else: 
-            return "inconclusive" 
+            return "neutral" 
 
-        for sentence in listOfStories:
-            tweets = sentence.split(",").strip()
+        
 
-            analyze_news(tweets)
-    
- #   hacker = input("What do you want to analyze?")
-  #  analyze_news(hacker)
-  #  print(analyze_news(hacker))
-#
-
-with open('/Users/MorsalNiyaz/Development/polyglot/cohort-1/data/positive_words.txt', 'r') as open_pos:
-    positive_vocab=open_pos.readlines()
-
-with open('/Users/MorsalNiyaz/Development/polyglot/cohort-1/data/negative_words.txt', 'r') as open_neg:
-    negative_vocab=open_neg.readlines()
-
-tn = TwitterNews(positive_vocab, negative_vocab)
-
-response = requests.get('https://api.twitter.com/1.1/search/tweets.json?q=%23{}&result_type=recent'.format(searchitem)')
-
-# Get top 5 ids
-top_five_ids = response.json()[:5]
-print(top_five_ids)
-
-
-
-# Fetch top 5 tweets 
-
-tweetsposted = []
-
-for top_id in top_five_ids:
-    curr_post_api_url = ''https://api.twitter.com/1.1/search/tweets.json?q=%23{}&result_type=recent'.format(top_id)
-    curr_post = requests.get(curr_post_api_url)
-    print(curr_post.json())
-    tweetsposted.append(curr_post.json())
-
-
-#print results
-for l in listOfTweets:
-    print(l)
-    print("\n")
