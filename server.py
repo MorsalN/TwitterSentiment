32a22e902f99f3829ca9cb55dd753cb8546da4ed
Morsal Niyaz <morsalniyaz@gmail.com>
2019-02-04 15:00:46 -0800

    twitter

--- server.py
+++ server.py
@@ -1,19 +1,65 @@
 from flask import Flask, request, render_template
 from templates.TAnalyzer import TwitterNews
 import requests
+import pandas as pd
+
+all_tweets = []
 
 app = Flask(__name__)
+
+with open('/Users/MorsalNiyaz/Development/polyglot/cohort-1/data/positive_words.txt', 'r') as open_pos:
+    positive_vocab=open_pos.readlines()
+    positive_vocab = list(map(lambda word: word.replace("\n", ""), positive_vocab)) #this take away the "/n" at the end of every tweet or comment
+
+with open('/Users/MorsalNiyaz/Development/polyglot/cohort-1/data/negative_words.txt', 'r') as open_neg:
+    negative_vocab=open_neg.readlines()
+    negative_vocab = list(map(lambda word: word[:-1], negative_vocab)) #another way of removing the "/n" at the end of a tweet
+
 
 @app.route('/')
 def my_form():
     return render_template('index.html')
 
-@app.route('/twitter', methods=['GET'])
-def my_form_post():
-    text = request.args.get('text')
-    currenttweets = get_tweets(text)
-    print(currenttweets)
-    return render_template('index.html', text=text, currenttweets=currenttweets)
+#@app.route('/twitter', methods=['GET'])
+#def my_form_post():
+
+    #text = request.args.get('text')
+    #currenttweets = get_tweets(text)
+    #print(currenttweets)
+    #return render_template('index.html', text=text, currenttweets=currenttweets)
+
+#this is a new route localhost:5000/tweets_list
+@app.route('/tweets_list', methods = ['GET','POST'])
+def post_dict():
+    if request.method == 'POST':
+        tweet = request.form['text']
+        all_tweets.append(tweet)
+    return render_template('tweet_dict.html', all_tweets=all_tweets)
+
+
+#reading files so that they don't delete every time after you restart 
+@app.route('/tweets_files', methods = ['GET','POST'])
+def post_files():
+    if request.method == 'POST':
+        tweet = request.form['text']
+        write_files(tweet)
+    read = read_files()
+    return render_template('saved_files.html', read = read)
+
+
+def read_files():
+    # Input: none
+    # Output: all tweets in tweets.txt
+    with open('tweets.txt', 'r') as f:
+        x = f.readlines()
+        return x
+
+
+def write_files(tweet):
+    # Input: tweet (str)
+    # Output: nothing
+    with open('tweets.txt', 'a+') as f:
+        f.write(tweet + "\n")
 
 
 #HELPER FUNCTION
@@ -26,25 +72,33 @@ def get_tweets(searchitem):
     
     tweetlist = [] #a list that will have the sentiment and the text for each tweet 
 
-    twitter_analyzer = TwitterNews() #you are calling the class TwitterNew from TAnalyzer.py
-    
+    twitter_analyzer = TwitterNews(positive_vocab, negative_vocab) #you are calling the class TwitterNew from TAnalyzer.py
+
     for item in status: #now we are trying to get the text that is inside the status 
         #print(item['text'])
         tweet = item['text']
-        sentimenttweet = twitter_analyzer.analyze_news(tweet) #you are calling the sentiment part from the TwitterNews class called analyze_news
+        
+        sentiment, percentagepos, percentageneg = twitter_analyzer.analyze_news(tweet) #you are calling the sentiment part from the TwitterNews class called analyze_news
         #print(sentimenttweet)
         # {'sentiment': 'positive', 'tweet': 'i love twitter'} -structure of a dictionary
         actualtweets = {}  #a dictionary
-        actualtweets["sentiment"] = sentimenttweet #the key - the right hand side is calling the sentiment tweets made from above 
-        actualtweets["tweet"] = tweet #the value
+        actualtweets["SENTIMENT"] = sentiment #the key - the right hand side is calling the sentiment tweets made from above 
+        actualtweets["TWEET"] = tweet #the value
+        actualtweets["POSPERCENT"] = percentagepos
+        actualtweets["NEGPERCENT"] = percentageneg
+
         #print(actualtweets) 
 
         tweetlist.append(actualtweets) #adding the acutaltweets dictionary into the tweetlist 
-
+   
     
     return tweetlist
+
+    data = pd.DataFrame(data=[actualtweets], columns=['Tweets'])
 
 
 if __name__ == '__main__':
   app.run() 
+
+
 
--- templates/TAnalyzer.py
+++ templates/TAnalyzer.py
@@ -1,47 +1,60 @@
 #Importing the top 5 stories and top 5 comments
-with open('/Users/MorsalNiyaz/Development/polyglot/cohort-1/data/positive_words.txt', 'r') as open_pos:
-    positive_vocab=open_pos.readlines()
-    positive_vocab = list(map(lambda word: word.replace("\n", ""), positive_vocab)) #this take away the "/n" at the end of every tweet or comment
-
-with open('/Users/MorsalNiyaz/Development/polyglot/cohort-1/data/negative_words.txt', 'r') as open_neg:
-    negative_vocab=open_neg.readlines()
-    negative_vocab = list(map(lambda word: word[:-1], negative_vocab)) #another way of removing the "/n" at the end of a tweet
-
-
 class TwitterNews:
-    def __init__(self):
+    def __init__(self, positive_vocab, negative_vocab):
         self.positive_vocab = positive_vocab #you are calling the vocab from above
         self.negative_vocab = negative_vocab
 
-    
-    def analyze_news(self, tweet):
-        
+    def clean_tweets(self, tweet):
+        """
+        Input: tweet (str)
+        Output: words (list) without #, @, $
+        """
         tweet = tweet.lower() #these are making the tweets on twitter lowercase, taking away #,@ etc. and replacing it with ""(which is blank)
         tweet = tweet.replace("#", "")  #so that our analyzer can give us an analysis without being confused
         tweet = tweet.replace("@","")
         tweet = tweet.replace("$", "")
-        words = tweet.split()
-        print(self.positive_vocab) #need to put the self.
-        print(self.negative_vocab)
+        return tweet
 
-        num_positive_vocab = 0 
-        num_negative_vocab = 0 
+    def analyze_news(self, tweet):
+        tweet = self.clean_tweets(tweet)
+        words = tweet.split()
+
+        num_positive_vocab = 0
+        num_negative_vocab = 0
+        percentagepos = 0
+        percentageneg = 0
+        sentiment = ""
+
         for word in words:
             if word in self.positive_vocab:
                 num_positive_vocab += 1 
             elif word in self.negative_vocab:
                 num_negative_vocab += 1 
-                print(num_negative_vocab)
-               
+                #print(num_negative_vocab)
+        total = num_positive_vocab + num_negative_vocab
+        #print("total: " + str(total))
+        try:
+            percentagepos = round((num_positive_vocab/total)*100,2)
+            percentageneg = round((num_negative_vocab/total)*100,2)
+        except ZeroDivisionError:
+            percentagepos = 0
+            percentageneg = 0
 
-        if num_positive_vocab > num_negative_vocab:
-            
-            return "positive"
+        print(percentageneg)
 
-        elif num_positive_vocab < num_negative_vocab:
-            return "negative"
-        else: 
-            return "neutral" 
+        if num_positive_vocab != num_negative_vocab:
+            if num_positive_vocab > num_negative_vocab:
+               # print("positive: " + str(percentage))
+                sentiment =  "Positive"
+            if num_positive_vocab < num_negative_vocab:
+                #total = len(positive_vocab) + len(negative_vocab)
+                
+                #print("negative: " + str(percentage))
+                sentiment =  "Negative"
+        else:
+            sentiment = "Neutral"
+
+        return sentiment, percentagepos, percentageneg
 
         
 
