fdde5521460acb2c1665c0266c78a917f3ddb803
Morsal Niyaz <morsalniyaz@gmail.com>
2019-02-11 14:22:22 -0800

    twitter

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ import sqlite3
 all_tweets = [] 
 
 app = Flask(__name__)
-conn = sqlite3.connect('twitter.db')
+conn = sqlite3.connect('twitter.db') #our database is twitter.db 
 
 c = conn.cursor()
 
@@ -19,20 +19,21 @@ c = conn.cursor()
 
 
 #this was added so you don't need to create one every single time it runs 
-c.execute("CREATE TABLE IF NOT EXISTS twittertable (id INTEGER PRIMARY KEY AUTOINCREMENT, tweet text)")
-#c.execute("CREATE TABLE IF NOT EXISTS tweettable (id INTEGER PRIMARY KEY AUTOINCREMENT, tweet text, FOREIGN KEY(user) REFERENCES user(id))")
-#c.execute("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, username text, password text)")
+#c.execute("CREATE TABLE IF NOT EXISTS twittertable (id INTEGER PRIMARY KEY AUTOINCREMENT, tweet text)") #creating a table with id and tweet - our id is an integer and we want it to be our primary key and we want to type in 
+c.execute("CREATE TABLE IF NOT EXISTS twittertable (id INTEGER PRIMARY KEY AUTOINCREMENT, tweet text, user_tweet text, FOREIGN KEY(user_tweet) REFERENCES user(id))")
+c.execute("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, username text, password text)")
+
 
 
 # Insert a row of data
 
 
-def insert(tweet, c):
-    query = "INSERT INTO twittertable(tweet) VALUES ('{}')".format(tweet)
-    print(query)
+def insert(tweet, c): #the ,c is calling the c from above so we don't need to keep typing it in all the time
+    query = "INSERT INTO twittertable(tweet) VALUES ('{}')".format(tweet) #the '{}' will format all the new tweets into the tweet section if we were to just type in tweet as the value then that is what it will print  
+    print(query) #looking at what it will print out 
     c.execute(query)
-@app.route('/twitter_clone', methods = ['GET','POST'])
-def insert_table():
+@app.route('/twitter_clone', methods = ['GET','POST']) 
+def insert_table(): #this is creating a funtion "def name():"
     conn = sqlite3.connect('twitter.db')
 
     c = conn.cursor()
@@ -42,15 +43,15 @@ def insert_table():
         tweet = request.form['text'] #getting the tweet through the form 
         insert(tweet, c)
         conn.commit()
-    get_comments = select_table(c) 
-    return render_template('twitter_clone.html', get_comments=get_comments)
+    get_comments = select_table(c) #you are calling the function from below so it will show all the tweets that were typed ie you are getting the tweets 
+    return render_template('twitter_clone.html', get_comments=get_comments) 
     
     
-def select_table(c):
+def select_table(c): #calling in the c from above 
     #input: none
     #output: tweets 
-    table = c.execute("select * from twittertable;")
-    return table.fetchall() 
+    table = c.execute("select * from twittertable;") #writing an sql statement showing all the attributes in the table 
+    return table.fetchall() #this is returning all the attributes in the table
 
 
 #tweets
@@ -81,7 +82,7 @@ def my_form():
 
 @app.route('/twitter', methods=['GET'])
 def my_form_post():
-    text = request.args.get('text')
+    text = request.args.get('text') #creating a variable called text
     currenttweets = get_tweets(text)
     #print(currenttweets)
     return render_template('index.html', text=text, currenttweets=currenttweets)
